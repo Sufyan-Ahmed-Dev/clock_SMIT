6fc8388d3a2c8ffb4291e246c79c927d51641e6b
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-25 20:03:36 +0500

    setInterval func run after every 1 sec

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // 2 = din k name show kary  ++ 
 // 3 month name show karana hai ++
 
-let date = new Date()
+function dateFunc(){
+    let date = new Date()
 let time = date.toLocaleTimeString()
 let otherData = date.toDateString()
 
@@ -10,3 +11,7 @@ document.querySelector("#time").innerHTML = time
 document.querySelector("#date").innerHTML = otherData
 console.log(time);
 console.log(otherData);
+}
+
+
+setInterval(dateFunc,1000)
