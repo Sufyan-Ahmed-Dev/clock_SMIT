cc9737e7d524e006f563ecf09139c5a2198c5c8a
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-25 08:37:10 +0500

    basic clock done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("Working")
+// 1 = 12 hours time set  ++
+// 2 = din k name show kary  ++ 
+// 3 month name show karana hai ++
+
+let date = new Date()
+let time = date.toLocaleTimeString()
+let otherData = date.toDateString()
+
+document.querySelector("#time").innerHTML = time
+document.querySelector("#date").innerHTML = otherData
+console.log(time);
+console.log(otherData);
